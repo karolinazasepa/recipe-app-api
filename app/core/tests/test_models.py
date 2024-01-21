e294b4b4cf5dae036a3d131c00dd7640138754ed
Karolina <karolina.zasepa16@gmail.com>
2024-01-21 22:12:28 +0100

    Fix test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,7 +23,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
-            ['test1@EXAMPLE.com', 'tes1@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
             ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
